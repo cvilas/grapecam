1b855e47f775d75f1e663b9d8b1b5a6ae50eda72
Vilas Chitrakaran <cvilas@gmail.com>
2025-01-08 16:10:36 +0000

    update SDL

--- external/third_party_versions.cmake
+++ external/third_party_versions.cmake
@@ -5,6 +5,6 @@
 # List of all third-party dependency versions
 
 set(LIBCAMERA_VERSION_REQUIRED  0.3.2)
-set(SDL3_VERSION_REQUIRED       "") # TODO: specify version
+set(SDL3_VERSION_REQUIRED       "3.1.8") # TODO: specify version
 set(GRAPE_VERSION_REQUIRED      "") # TODO: specify version
 set(GRAPE_MODULES               "app")
